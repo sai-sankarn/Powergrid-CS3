a78a0dd80c8c6cf4503d6a9b617b0c3a32ceb57b
sai-sankarn <k1627910@students.katyisd.org>
2026-03-06 09:29:14 -0600

    bidding panel new

--- Frontend/StartPanel.java
+++ Frontend/StartPanel.java
@@ -1,25 +1,53 @@
 package Frontend;
 
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-public class StartPanel extends JPanel{
+public class StartPanel extends JPanel implements MouseListener{
     BufferedImage image;
+    PowergridFrame frame;
 
-    public StartPanel(){
+    public StartPanel(PowergridFrame frame){
         try {
             image = ImageIO.read(StartPanel.class.getResource("/Images/powergridTitle.png"));
         } catch (Exception e) {
             System.out.println("EXCEPTION ERRORRR");
         }
-
+        this.frame = frame;
+        addMouseListener(this);
         repaint();
     }
 
     public void paint(Graphics g){
         super.paint(g);
         g.drawImage(image, 0, 0, 1600, 1000, null);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("(" + e.getX() + ", " + e.getY() + ")");
+        if (e.getX()>1244 && e.getX()< 1467 && e.getY()>421 && e.getY() < 495){
+            frame.showScreen("BIDDING");
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
     }
 }
