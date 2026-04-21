6fcd81d6e6216a9655bd39add3d1514b7cbd8467
Catherine Wang <K1223048@students.katyisd.org>
2026-04-21 09:23:32 -0500

    ACTUALLY done with player order!!

--- Frontend/PlayerOrderPanel.java
+++ Frontend/PlayerOrderPanel.java
@@ -1,11 +1,14 @@
 package Frontend;
 
+import Backend.Player;
+import Backend.RoundManager;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
+import java.util.List;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
@@ -14,11 +17,16 @@ public class PlayerOrderPanel extends JPanel implements MouseListener {
     private BufferedImage background;
     private PowergridFrame frame;
 
+    private RoundManager rm;
+    private List<Player> allPlayers;
+
     private JButton continueButton;
 
     public PlayerOrderPanel(PowergridFrame frame) {
         this.frame = frame;
         setLayout(null);
+        rm = frame.getRoundManager();
+        allPlayers = rm.getPlayers();
 
         try {
             background = ImageIO.read(getClass().getResource("/Images/background.png"));
@@ -50,7 +58,7 @@ public class PlayerOrderPanel extends JPanel implements MouseListener {
         g.setColor(Color.WHITE);
 
         g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
-        g.drawString("STEP 1, PHASE 1: PLAYER ORDER", 900, 49);
+        g.drawString("STEP " + rm.getCurrentStep() + ", PHASE 1: PLAYER ORDER", 900, 49);
 
         g.setColor(new Color(67, 126, 161));
         g.fillRect(980, 120, 500, 200);
@@ -64,12 +72,12 @@ public class PlayerOrderPanel extends JPanel implements MouseListener {
         g.drawRect(980, 560, 500, 200);
 
         g.setFont(new Font("Arial", Font.BOLD, 40));
-        g.drawString("Player 1: 1 cities", 1000, 190);
-        g.drawString("Biggest power plant: 1", 1000, 260);
-        g.drawString("Player 2: 2 cities", 1000, 410);
-        g.drawString("Biggest power plant: 2", 1000, 480);
-        g.drawString("Player 3: 3 cities", 1000, 630);
-        g.drawString("Biggest power plant: 3", 1000, 700);
+        g.drawString("Player 1: "+allPlayers.get(0).getCityCount() +" cities", 1000, 190);
+        g.drawString("Biggest power plant: "+allPlayers.get(0).getHighestPlantNumber(), 1000, 260);
+        g.drawString("Player 2: "+allPlayers.get(1).getCityCount()+" cities", 1000, 410);
+        g.drawString("Biggest power plant: "+allPlayers.get(1).getHighestPlantNumber(), 1000, 480);
+        g.drawString("Player 3: "+allPlayers.get(2).getCityCount()+" cities", 1000, 630);
+        g.drawString("Biggest power plant: "+allPlayers.get(2).getHighestPlantNumber(), 1000, 700);
     }
 
     private void endOrderPhase() {
